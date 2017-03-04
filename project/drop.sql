106e269acaf10846718f0e24eb12b955745f5586
bekaagava <bekagava@gmail.com>
2017-03-03 23:53:30 -0500

    Design submission

---
+++ project/drop.sql
@@ -0,0 +1,16 @@
+drop table OwnerStock;
+drop table RecipeIngredient;
+drop table OwnerRecipe;
+drop table Stock;
+drop table Ingredient;
+drop table Recipe;
+drop table Owner;
+drop table UserType;
+drop table Fridge;
+
+drop sequence fridge_sequence;
+drop sequence userType_sequence;
+drop sequence owner_sequence;
+drop sequence recipe_sequence;
+drop sequence ingredient_sequence;
+drop sequence stock_sequence;
