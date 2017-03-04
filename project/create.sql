ed74b0bb35fcf5aa920bd5e974f7dcd6f5fcbd35
bekaagava <bekagava@gmail.com>
2017-03-03 23:52:39 -0500

    Creates the relations

---
+++ project/create.sql
@@ -0,0 +1,26 @@
+-- Create the RFID Fridge user and database. 
+-- See ../README.txt for details.
+
+-- Create the user.
+DROP USER baa8 CASCADE;
+CREATE USER baa8 
+	IDENTIFIED BY seven
+	QUOTA 5M ON System;
+GRANT 
+	CONNECT,
+	CREATE TABLE,
+	CREATE SESSION,
+	CREATE SEQUENCE,
+	CREATE VIEW,
+	CREATE MATERIALIZED VIEW,
+	CREATE PROCEDURE,
+	CREATE TRIGGER,
+	UNLIMITED TABLESPACE
+	TO baa8;
+
+-- Connect to the user's account/schema.
+CONNECT baa8/seven;
+
+-- Create the database.
+DEFINE baa8=S:\cs342\project
+@&baa8\load
