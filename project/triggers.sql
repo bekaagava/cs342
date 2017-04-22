44670b803275bd67b6b3368af4b2a7cde2e01271
bekaagava <bekagava@gmail.com>
2017-04-22 14:59:27 -0400

    Updated triggers

--- project/triggers.sql
+++ project/triggers.sql
@@ -1,34 +1,75 @@
 -- Triggers for the RFID database
 -- 04/21/2017
 
-/* This checks to see make sure the new ingredient does not already exist in the 
-table. This is hard to check with a regular table constraint
+/* This checks to see make sure the new stock for the fridge is not expired. Since check constraints 
+have to be deterministic and sysdate is non-deterministic, the isExpired trigger is used to check inserts.
 */
 
 SET SERVEROUTPUT ON;
 
-CREATE OR REPLACE TRIGGER ingredientExistsTrigger BEFORE INSERT ON Ingredient FOR EACH ROW
+DROP TRIGGER ingredientExistsTrigger;
+
+CREATE OR REPLACE TRIGGER isExpiredTrigger BEFORE INSERT ON Stock FOR EACH ROW
 DECLARE
-counter INTEGER;
-alreadyExists_exception EXCEPTION;
+
+isExpired_exception EXCEPTION;
 
 BEGIN
-	SELECT COUNT(*) INTO counter
-	FROM Ingredient i
-	WHERE i.name = :new.name;
 	
-	IF counter > 0 THEN
-		RAISE alreadyExists_exception;
+	IF :new.expirationDate < SYSDATE THEN
+		RAISE isExpired_exception;
 	END IF;
 	
 EXCEPTION
-	WHEN alreadyExists_exception THEN
-	RAISE_APPLICATION_ERROR(-20001, 'This ingredient already exists!');
+	WHEN isExpired_exception THEN
+	RAISE_APPLICATION_ERROR(-20001, 'This stock is expired!');
 END;
 /
 
--- Test: This should fail
+/* This trigger checks to make sure that the stock is being put in the correct 
+fridge, that is the fridge assigned to the owner matches the fridge assigned to 
+the stock
+*/
+
+CREATE OR REPLACE TRIGGER wrongFridgeTrigger BEFORE INSERT ON OwnerStock FOR EACH ROW
+DECLARE
+
+counter INTEGER;
+invalid_exception EXCEPTION;
+
+BEGIN
+	
+	SELECT COUNT(*) INTO counter
+	FROM Stock s, Owner o
+	WHERE :new.ownerID = o.id
+	AND :new.stockID = s.RFID
+	AND s.fridgeID = o.fridgeID;
+
+	IF counter < 1 THEN
+		RAISE invalid_exception;
+	END IF;
+	
+EXCEPTION
+	WHEN invalid_exception THEN
+	RAISE_APPLICATION_ERROR(-20001, 'This stock is being placed in the wrong fridge!');
+END;
+/
+
+
+-- Test: This should fail because the stock is already expired
 BEGIN 
-	INSERT INTO Ingredient VALUES(500, 'chicken', 'food');
+	INSERT INTO Stock VALUES(500, '03-Mar-2017', 107, 112);
+END;
+/
+
+-- This should work because the fridge and the owner match
+BEGIN 
+	INSERT INTO OwnerStock VALUES(100, 104);
+END;
+/
+
+-- This should not work because the fridge and the owner do not match
+BEGIN 
+	INSERT INTO OwnerStock VALUES(100, 130);
 END;
 /
